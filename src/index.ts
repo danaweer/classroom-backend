4231528e9bfc86163abc7f5783c251ed96be08d9
danaweer <dodiwer2001@gmail.com>
2026-03-10 04:26:50 +0200

    implement GET all subjects route

--- src/index.ts
+++ src/index.ts
@@ -1,57 +1,23 @@
-import { eq } from "drizzle-orm";
-import { db, pool } from "./db";
-import { departments } from "./db/schema";
+import express from 'express';
+import subjectsRouter from './routes/subjects';
+import cors from 'cors';
 
-async function main() {
-  try {
-    console.log("Performing CRUD operations...");
+const app = express();
+const port = 8000;
 
-    const [newDepartment] = await db
-      .insert(departments)
-      .values({
-        code: "CS",
-        name: "Computer Science",
-        description: "Department for computing and software.",
-      })
-      .returning();
+app.use(cors({
+    origin: process.env.FRONTEND_URL , // Allow requests from the frontend
+    methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed HTTP methods
+    credentials: true, // Allow cookies to be sent with requests
+}));
 
-    if (!newDepartment) {
-      throw new Error("Failed to create department");
-    }
+app.use(express.json());
+app.use('/api/subjects', subjectsRouter) 
 
-    console.log("CREATE:", newDepartment);
+app.get('/', (req, res) => {
+  res.send('Hello, there!');
+});
 
-    const foundDepartment = await db
-      .select()
-      .from(departments)
-      .where(eq(departments.id, newDepartment.id));
-    console.log("READ:", foundDepartment[0]);
-
-    const [updatedDepartment] = await db
-      .update(departments)
-      .set({ name: "Computer Science & Engineering" })
-      .where(eq(departments.id, newDepartment.id))
-      .returning();
-
-    if (!updatedDepartment) {
-      throw new Error("Failed to update department");
-    }
-
-    console.log("UPDATE:", updatedDepartment);
-
-    await db.delete(departments).where(eq(departments.id, newDepartment.id));
-    console.log("DELETE: Department deleted.");
-
-    console.log("CRUD operations completed successfully.");
-  } catch (error) {
-    console.error("Error performing CRUD operations:", error);
-    process.exit(1);
-  } finally {
-    if (pool) {
-      await pool.end();
-      console.log("Database pool closed.");
-    }
-  }
-}
-
-main();
+app.listen(port, () => {
+  console
+});
